62290ed1d6fc2a01a1bce3db54275c4209b2c3ed
Sandip <sandeep47nepali@gmail.com>
2022-08-08 23:16:58 +0545

    DOM Drilling

---
+++ DOM/app.js
@@ -0,0 +1,5 @@
+// let anchorElement = document.getElementById('external-link');
+// anchorElement.href='https://google.com';
+
+let anchorElement = document.querySelector('a');
+anchorElement.href = 'https://www.w3schools.com/';
